76014c33e48677ddb0839851ed0b70514c0e8d9e
matty0123 <wiggawolf@gmail.com>
2026-03-17 18:01:37 +0100

    Update ExplorationPipelineUpdated.py

--- ExplorationPipelineUpdated.py
+++ ExplorationPipelineUpdated.py
@@ -18,7 +18,7 @@ chunksize = 10000
 
 #before cleaning:
 word_counter_raw = Counter()
-print("Processing RAW text...")
+print("Raw text:")
 
 for chunk in pd.read_csv(
     "995,000_rows.csv",
@@ -32,11 +32,11 @@ for chunk in pd.read_csv(
         tokens = nltk.word_tokenize(text.lower())
         word_counter_raw.update(tokens)
 
-print("\nTop 100 words BEFORE cleaning:")
+print("\nTop 100 words before cleaning:")
 print(word_counter_raw.most_common(100))
 
 #after cleaning
-print("\nProcessing CLEANED text...")
+print("\nProcessing cleaned text...")
 
 df_clean = pd.read_csv("cleaned_for_classification.csv")
 texts_clean = df_clean["cleaned_text"].dropna().astype(str)
@@ -51,6 +51,5 @@ print("URL count (cleaned):", clean_word_counter["url"])
 print("DATE count (cleaned):", clean_word_counter["date"])
 print("NUMBER count (cleaned):", clean_word_counter["number"])
 
-top_100_clean = clean_word_counter.most_common(100)
-print("\nTop 100 words AFTER cleaning:")
-print(top_100_clean)
+print("\nTop 100 words after cleaning:")
+print(clean_word_counter.most_common(100))
